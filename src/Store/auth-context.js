12e1d75d3403cf3ed45815a393336b9f9ba10ef9
Nizamph <116118003+Nizamph@users.noreply.github.com>
2023-01-25 21:48:51 +0530

    persisted user authentication

--- src/Store/auth-context.js
+++ src/Store/auth-context.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 
 const AuthContext = React.createContext({
   token: '',
@@ -9,16 +9,24 @@ const AuthContext = React.createContext({
 
 
 export const AuthContextProvider = (props) => {
-    const [token, setToken] = useState(null)
+    const initialToken = localStorage.getItem('token');
+    const [token, setToken] = useState(initialToken)
 
     const useIsloggedIn = !!token; //if token is a string not empty this is will give true otherwise this will give false
 
-
+ useEffect(() => {
+  
+ })
     const loginHandler  = (token) => {
       setToken(token);
+    
+      localStorage.setItem('token',token)
+      
     };
     const logoutHandler = () => {
         setToken(null);
+      localStorage.removeItem('token'); 
+      
     }
 
     const contextValue = {
